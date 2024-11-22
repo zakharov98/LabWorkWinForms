b136134a348563998c12b9237c17098b7cf3077f
D.Zakharov <d.a.zakharov98@gmail.com>
2024-11-22 06:30:07 +0300

    first commit

---
+++ LabWorkWinForms/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace LabWorkWinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите натуральное число:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(169, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(257, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Найти сумму цифр";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 63);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(257, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(169, 103);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Введите натуральное число:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 158);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(257, 20);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 129);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(257, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Найти минимум и максимум";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(169, 199);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 202);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Введите натуральное число:";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(12, 254);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(257, 20);
+            this.textBox6.TabIndex = 11;
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 225);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(257, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Найти простые числа";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(12, 298);
+            this.textBox7.Multiline = true;
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox7.Size = new System.Drawing.Size(257, 61);
+            this.textBox7.TabIndex = 13;
+            this.textBox7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 269);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(257, 23);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Решить ребус";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 525);
+            this.splitter1.TabIndex = 14;
+            this.splitter1.TabStop = false;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(12, 398);
+            this.textBox8.Multiline = true;
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox8.Size = new System.Drawing.Size(257, 115);
+            this.textBox8.TabIndex = 16;
+            this.textBox8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 369);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(257, 23);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "Решить гусиков и кролей";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(283, 525);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ LabWorkWinForms/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabWorkWinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int CalculateDigitSum(int n)
+        {
+            int sum = 0;
+            while (n != 0)
+            {
+                sum += n % 10;
+                n /= 10;
+            }
+            return sum;
+        }
+
+        private (int max, int min) FindMaxMinDigits(int n)
+        {
+            int max = n % 10;
+            int min = n % 10;
+            n /= 10;
+
+            while (n != 0)
+            {
+                int a = n % 10;
+
+                if (a > max)
+                    max = a;
+                else if (a < min)
+                    min = a;
+
+                n /= 10;
+            }
+
+            return (max, min);
+        }
+        private int[] GetPrimeNumbersInRange(int start, int end)
+        {
+            var primes = new System.Collections.Generic.List<int>();
+
+            for (int x = start; x <= end; x++)
+            {
+                bool isPrime = true;
+
+                for (int del = 2; del * del <= x; del++)
+                {
+                    if (x % del == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime)
+                {
+                    primes.Add(x);
+                }
+            }
+
+            return primes.ToArray();
+        }
+
+        private string SolveRebus()
+        {
+            for (int M = 1; M <= 9; M++)
+                for (int U = 0; U <= 9; U++)
+                    for (int H = 0; H <= 9; H++)
+                        for (int A = 1; A <= 9; A++)
+                            for (int S = 3; S <= 9; S++)
+                                for (int L = 0; L <= 9; L++)
+                                    for (int O = 0; O <= 9; O++)
+                                        for (int N = 0; N <= 9; N++)
+                                        {
+                                            int[] digits = { M, U, H, A, S, L, O, N };
+                                            if (digits.Distinct().Count() == digits.Length)
+                                            {
+                                                int MUHA = 1000 * M + 100 * U + 10 * H + A;
+                                                int SLON = 1000 * S + 100 * L + 10 * O + N;
+
+                                                if (3 * MUHA == SLON)
+                                                {
+                                                    return $"Решение найдено:МУХА = {MUHA}, СЛОН = {SLON}, " +
+                                                           $"М = {M}, У = {U}, Х = {H}, А = {A}, С = {S}, Л = {L}, О = {O}, Н = {N}";
+                                                }
+                                            }
+                                        }
+
+            return "Решение не найдено.";
+        }
+
+        public static List<Tuple<int, int>> FindCombinations()
+        {
+            List<Tuple<int, int>> combinations = new List<Tuple<int, int>>();
+
+            for (int y = 0; y <= 16; y++)
+            {
+                int x = 32 - 2 * y;
+
+                if (x >= 0)
+                {
+                    combinations.Add(new Tuple<int, int>(x, y));
+                }
+            }
+
+            return combinations;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (int.TryParse(textBox1.Text, out int number) && number >= 0)
+            {
+                int sum = CalculateDigitSum(number);
+                textBox2.Text = $"Сумма цифр: {sum}";
+            }
+            else
+            {
+                MessageBox.Show("Введите корректное натуральное число.", "Ошибка ввода", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if (int.TryParse(textBox3.Text, out int number) && number > 0)
+            {
+                (int max, int min) = FindMaxMinDigits(number);
+                textBox4.Text = $"Наибольшая: {max}, Наименьшая: {min}";
+            }
+            else
+            {
+                MessageBox.Show("Введите корректное натуральное число.", "Ошибка ввода", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox5.Text, out int n) && n > 2)
+            {
+                int[] primes = GetPrimeNumbersInRange(2, n);
+
+                if (primes.Length > 0)
+                {
+                    textBox6.Text = "[" + string.Join(", ", primes) + "]";
+                }
+                else
+                {
+                    textBox6.Text = "Простых чисел в этом диапазоне нет.";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите корректное число больше 2.", "Ошибка ввода", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string result = SolveRebus();
+
+            textBox7.Text = result;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            var combinations = FindCombinations();
+            textBox8.Clear();
+
+            foreach (var combination in combinations)
+            {
+                textBox8.AppendText($"Гусей: {combination.Item1}, Кроликов: {combination.Item2}\r\n");
+            }
+        }
+    }
+}
